df87ef3ed515192b0a63087f0ffa00325c05e8a8
Sonali Karangutkar(Dataanalyst2) <dataanalyst2.mum@executiveship.com>
2025-11-13 20:31:00 +0530

    Fix vite.config.js syntax for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', 
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']], 
+        plugins: [['babel-plugin-react-compiler']],
       },
     }),
     tailwindcss({
       theme: {
         container: {
           center: true,
-          padding: "1rem",
+          padding: '1rem',
           screens: {
-            sm: "100%",
-            md: "100%",
-            lg: "100%",
-            xl: "100%",
-            "2xl": "100%",
+            sm: '100%',
+            md: '100%',
+            lg: '100%',
+            xl: '100%',
+            '2xl': '100%',
           },
         },
       },
     }),
   ],
+  build: {
+    chunkSizeWarningLimit: 2000, 
+  },
 })
